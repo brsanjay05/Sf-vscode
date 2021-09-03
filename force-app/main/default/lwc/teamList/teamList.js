b860c2b169600752a2ba2ace207c2fd194f42817
brsanjay05 <sanjay.br@veneratesolutions.com>
2021-09-04 00:09:34 +0530

    Revert "Sample code"
    This reverts commit 6e0723141b3c40d4c6f70cc9ff66e3ebf8bf1ebc.

--- force-app/main/default/lwc/teamList/teamList.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class teamList extends LightningElement {
-    @api recordId
-    objectApiName = 'Team__c'
-    fields = ['Name', 'Team_Coach__c', 'Captain_Name__c']
-}
--- force-app/main/default/triggers/RestrictContactByName.trigger
+++
@@ -1,3 +0,0 @@
-trigger RestrictContactByName on Contact (before insert) {
-
-}
